a2a4f23aef17696a8bf18ff2c3034b5daa47f21d
DylanRamirezV <dyldarava4@gmail.com>
2026-05-31 17:02:59 +0000

    database

--- database.py
+++ database.py
@@ -8,16 +8,19 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Creamos el motor de la base de datos
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL) if DATABASE_URL else None
 
 # Sesión local para interactuar con la BD
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) if engine else None
 
 # Base para nuestros modelos
 Base = declarative_base()
 
 # Dependencia para abrir/cerrar la BD en cada petición
 def get_db():
+    if SessionLocal is None:
+        raise RuntimeError("DATABASE_URL no esta configurada.")
+
     db = SessionLocal()
     try:
         yield db
